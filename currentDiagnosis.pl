21dd354363cffc75bd125d3bdb1c342c135fc7aa
jh1662 <jh1662@canterbury.ac.uk>
2024-04-22 13:39:57 +0100

    a start - trying to determine if one has diabetes

---
+++ currentDiagnosis.pl
@@ -0,0 +1,18 @@
+glucoseFasting(A) :- A >= 126.
+%^ Fasting plasma glucose level test - positive when X >= 126 mg/dL
+glucoseCurrent(A) :- A >= 200.
+%^ Current plasma glucose level test - positive when X >= 200 mg/dL
+glycatedHemoglobin(A) :- A >= 48.
+%^ Glycated hemoglobin test - positive when X >= 48 mmol/mol
+
+hasDiabetes(A) :- glucoseFasting(A), glucoseCurrent(A), glycatedHemoglobin(A).
+%^ can be either type 1 or 2
+
+%: To differ from type 1 and 2 (or both)
+
+%: source - https://www.mayoclinic.org/diseases-conditions/type-1-diabetes/diagnosis-treatment/drc-20353017#:~:text=If%20you're%20diagnosed%20with,the%20diagnosis%20isn't%20certain.
+urineHasKetones(A) :- A=1.
+autoAntibodiesPresent(A) :- A=1.
+hasType1(A) :- urineHasKetones(A), autoAntibodiesPresent(A).
+
+% https://www.swi-prolog.org/download/stable <-- use this as website IDE has "code limitations"
