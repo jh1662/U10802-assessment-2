09699d04181c9194fd62188f559f2bdb672df1cb
jh1662 <JH1662@canterbury.ac.uk>
2024-05-02 16:35:54 +0100

    cant seem to catch the N value outside of scope

--- symptoms.pl
+++ symptoms.pl
@@ -1,5 +1,6 @@
 % All possible symptoms
-%% symptom("polyuria", "thirst", "weight loss", "fatigue", "blurred vision", "genital itchiness").
+%% :- initialization(body_code).
+
 symptom("polyuria").
 symptom("thirst").
 symptom("weight loss").
@@ -7,28 +8,37 @@ symptom("fatigue").
 symptom("blurred vision").
 symptom("ginital itchiness").
 
+% initialiser predicate
 body_code :-
-    write('smth'),
+    write('Starting program'),
     ask_symptoms,
+    loop(0),
     ask_again(N),
-    dictateSeverity(N),
+    dictate_severity(N)
+    .
 
+% introduction
 ask_symptoms :-
     write('Please enter your symptoms, one per line in speech marks.'), nl,
     write('Respond with "done" when you finished (not including the speech marks)'), nl,
     write('All possible symptoms: "polyuria", "thirst", "weight loss", "fatigue", "blurred vision", "genital itchiness"'), nl,
-    write('weighting of a symptom can be increased if you entered it multiple times'), nl,
-    loop(0).
+    write('weighting of a symptom can be increased if you entered it multiple times'), nl
+    .
 
 % incase user made a mistake:
 ask_again(N) :-
     write('Do you want to redo (otherwise continue) [y/n]'), nl,
     read(Decision),
+    %% (Decision = y -> ask_symptoms(N);
+    %% Decision = n -> dictate_severity(N);
+    %% write('invalid answer - enter either: y or n'), nl, ask_again(N)
+    %% ).
     (Decision = y -> ask_symptoms;
-    Decision = n -> dictateSeverity(N);
+    Decision = n -> write('calculating');
     write('invalid answer - enter either: y or n'), nl, ask_again(N)
     ).
 
+% iterate over each symptom input
 loop(N) :-
     read(Symptom),
     ( Symptom = done ->
@@ -39,5 +49,9 @@ loop(N) :-
     ; loop(N), write(Symptom), write(' - symptom doesnt match existing syptoms or is mispelled'), nl
     ).
 
+% calculate and output result
 dictate_severity(N) :-
+    write('number:'), ln,
     write(N).
+
+
